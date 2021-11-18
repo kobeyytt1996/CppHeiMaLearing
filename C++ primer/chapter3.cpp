9d6e532649698298f6100a6e3b5d8ce91d7f18e3
yuanyuan02 <13718503098@163.com>
2021-11-18 20:35:59 +0800

    string,vector

--- C++ primer/chapter3.cpp
+++ C++ primer/chapter3.cpp
@@ -2,6 +2,8 @@
 #include <string>
 using namespace std;
 #include <cctype>
+#include <vector>
+
 
 int main() {
 	string s1, s2;
@@ -10,10 +12,44 @@ int main() {
 	decltype(s1.size()) i1;
 	s1 = s2 + "hello" + '\n' + "\"";
 
-	for (auto c : s1) {
+	for (auto &c : s1) {
 		if (ispunct(c)) {
 			cout << c << endl;
 		}
+		c = toupper(c);
+	}
+	s1[0] = tolower(s1[0]);
+
+	for (auto& c : s1) {
+		c = 'X';
+	}
+
+	const string s3("http");
+	for (auto& c : s3) {
+
+	}
+
+	vector<string> v1{"a", "b"};
+	vector<string> svec(20);
+
+	cout << s1 << endl;
+
+	vector<int> vi1;
+	int i2 = 0;
+	for (auto& i4 : vi1) {
+		i4 *= i4;
+	}
+
+	vector<string> vs2;
+	string word;
+	while (cin >> i2) {
+		cout << i2 << endl;
+	}
+
+	auto b1 = vs2.begin(), b2 = vs2.end();
+
+	for (auto it = vs2.cbegin(); it != vs2.cend(); ++it) {
+
 	}
 
 	return 0;
