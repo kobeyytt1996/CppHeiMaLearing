fcc689038aef7fcb619d5cc80570e7d3bafd50db
yuanyuan02 <13718503098@163.com>
2021-11-22 13:09:54 +0800

    第五章

--- C++ primer/chapter5.cpp
+++ C++ primer/chapter5.cpp
@@ -9,15 +9,29 @@ void printNum(unsigned int ui) {
 	}
 }
 
-int main() {
+int main05() {
 	double d = 0.0, d1 = 0.1;
 	vector<int> vi1;
 	for (decltype(vi1.size()) i = 0, j = vi1.size(); i != j; ++i) {
 
 	}
-	char ch = '\0';
+	/*char ch = '\0';
 	while (!(cin >> ch).eof()) {
 		cout << 1 << endl;
+	}*/
+
+	int i1 = 0, i2 = 0;
+	while (cin >> i1 >> i2)
+	{
+		try {
+			if (i2 == 0) {
+				throw runtime_error("³ıÊıÎª0");
+			}
+			cout << i1 / i2 << endl;
+		}
+		catch (runtime_error re) {
+			cout << re.what() << endl;
+		}
 	}
 
 	return 0;
