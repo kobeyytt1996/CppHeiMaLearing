e638c141df419ce962f206f2cf63a1d9bb461aa0
yuanyuan02 <13718503098@163.com>
2021-11-28 00:07:56 +0800

    第八章class完结

--- C++ primer/Screen.h
+++ C++ primer/Screen.h
@@ -1,34 +1,84 @@
-#pragma once
-#include <string>
-#include <vector>
-
-class Screen {
-public:
-	using pos = std::string::size_type;
-
-	Screen() = default;
-
-	Screen(pos wd, pos ht, char c) : width(wd), height(ht), contents(wd*ht, c) {}
-
-	char get() const {
-		++count;
-		return contents[cursor];
-	}
-
-	inline char get(pos ht, pos wd) const;
-
-	Screen& move(pos r, pos c);
-
-private:
-	pos cursor = 0;
-	pos height = 0, width = 0;
-	std::string contents;
-	mutable pos count = 0;
-};
-
-class Window {
-public:
-	std::vector<Screen> screens{ Screen(10, 20, ' ') };
-
-	Screen screen{ 10, 20, 'a' };
-};
+//#pragma once
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//
+//int height;
+//const int ci = 20;
+//
+//class Window {
+//public:
+//	using ScreenType = std::vector<int>::size_type;
+//	std::vector<Screen> screens{ Screen(10, 20, ' ') };
+//
+//	Screen screen{ 10, 20, 'a' };
+//
+//	ScreenType get();
+//};
+//
+//class Screen {
+//	// friend class Window;
+//	friend Window::ScreenType Window::get();
+//public:
+//	using pos = std::string::size_type;
+//
+//	Screen() = default;
+//
+//	Screen(pos wd, pos ht, char c) : width(wd), height(ht), contents(wd*ht, c) {}
+//
+//	char get() const {
+//		++count;
+//		return contents[cursor];
+//	}
+//
+//	inline char get(pos ht, pos wd) const;
+//
+//	const Screen& move(pos r, pos c) const {
+//		return *this;
+//	}
+//
+//	const Screen& display() const {
+//		do_display();
+//		return *this;
+//	}
+//
+//	Screen& display() {
+//		do_display();
+//		return *this;
+//	}
+//
+//private:
+//	pos cursor = 0;
+//	pos height = 0, width = 0;
+//	std::string contents;
+//	mutable pos count = 0;
+//
+//	void do_display() const {
+//
+//	}
+//};
+//
+//char Screen::get(pos ht, pos wd) const {
+//	::height;
+//	return ' ';
+//}
+//
+//Window::ScreenType Window::get() {
+//	screen.cursor;
+//}
+//
+//class Y;
+//
+//class X {
+//	Y* yPtr;
+//};
+//
+//class Y {
+//	X m_x;
+//};
+//
+//typedef double Money;
+//class Accout {
+//public:
+//	typedef double Money;
+//};
--- C++ primer/chapter7.cpp
+++ C++ primer/chapter7.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 #include "Person.h"
+#include <vector>
+#include <string>
 
 struct Sales_data {
 	string isbn() const {
@@ -18,8 +20,78 @@ double Sales_data::avg_price() const {
 	return 0.0;
 }
 
-int main() {
+class Const_Ref {
+public:
+	Const_Ref(int ii);
+	Const_Ref();
+private:
+	const int ci;
+	const int& ri;
+};
 
+Const_Ref::Const_Ref(int ii) : ci(ii), ri(ci) {
 
+}
+
+Const_Ref::Const_Ref() : Const_Ref(10) {
+
+}
+
+class TypeCon {
+public:
+	explicit TypeCon(int i) {
+		record = i;
+	}
+
+	int record;
+};
+
+struct Aggregate {
+	string s;
+	int i;
+};
+
+class Debug {
+public:
+	bool io;
+
+	constexpr Debug(bool b) : io(b) {}
+
+};
+
+class Account {
+public:
+	static double rate() {
+		return 0.0;
+	}
+private:
+	static double interestRate;
+
+	static constexpr double scd = 0.0;
+
+	static Account acc;
+};
+
+double Account::interestRate = 0.0;
+constexpr double Account::scd;
+
+int main07() {
+	Const_Ref cr = 5;
+	
+	/*TypeCon tc = 5;
+	tc = 6;
+	tc = static_cast<int>(7.5F);*/
+
+	TypeCon tc = static_cast<TypeCon>(5);
+
+	cout << tc.record << endl;
+
+	string s = "aa";
+	vector<int> vi = static_cast<vector<int>>(5);
+
+	Aggregate a{ "aa", 2 };
+	Aggregate a1 = { "aa", 2 };
+	
+	double d1 = Account::rate();
 	return EXIT_SUCCESS;
 }
